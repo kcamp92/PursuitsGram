7552e6f5da56d2ccfc530e7117489bd80f1834a4
Krystal Campbell <krystalcampbell@Krystals-MacBook-Pro.local>
2019-11-22 12:24:03 -0500

    initial commit

---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'PursuitsGram' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for PursuitsGram
+
+  target 'PursuitsGramTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ PursuitsGram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PursuitsGram
+//
+//  Created by Krystal Campbell on 11/22/19.
+//  Copyright © 2019 Krystal Campbell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
