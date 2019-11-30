aaf88d56a578c3dcee72afdedeef299d7a026aa7
Krystal Campbell <krystalcampbell@Krystals-MBP.fios-router.home>
2019-11-30 10:37:35 -0500

    adds objc functions and private methods

--- PursuitsGram/Controllers/Users/ProfileEditViewController.swift
+++ PursuitsGram/Controllers/Users/ProfileEditViewController.swift
@@ -20,6 +20,8 @@ class ProfileEditViewController: UIViewController {
 
     var imageURL: URL? = nil
     
+//MARK:- UI Objects
+    
     lazy var imageView: UIImageView = {
          let iv = UIImageView()
          iv.backgroundColor = .black
@@ -38,7 +40,7 @@ class ProfileEditViewController: UIViewController {
           button.addTarget(self, action: #selector(addImagePressed), for: .touchUpInside)
           return button
       }()
-
+    
     lazy var userNameTextField: UITextField = {
          let textField = UITextField()
          textField.placeholder = "Enter User Name"
@@ -48,11 +50,13 @@ class ProfileEditViewController: UIViewController {
          textField.borderStyle = .bezel
          textField.layer.cornerRadius = 5
          textField.autocorrectionType = .no
-         textField.delegate = self
+        // textField.delegate = self
          textField.textColor = .white
          return textField
      }()
-  
+
+
+
     lazy var saveButton: UIButton = {
         let button = UIButton()
         button.setTitle("Save Profile", for: .normal)
@@ -77,15 +81,102 @@ class ProfileEditViewController: UIViewController {
         return button
     }()
     
+//MARK: - Lifecycle Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .darkGray
 
      
     }
     
-
+//MARK: -Objc Methods
+    
+    @objc private func savePressed(){
+           guard let userName = userNameTextField.text, let imageURL = imageURL else {
+             showAlert(with: "Failure", and: "Profile Not Updated")
+               return
+           }
+        FirebaseAuthService.manager.updateUserFields(userName: userName, photoURL: imageURL) { (result) in
+            switch result {
+            case .success():
+                FirestoreService.manager.updateCurrentUser(userName: userName, photoURL: imageURL) { [weak self] (nextResult) in
+                    switch nextResult {
+                    case .success():
+                        self?.handleNavAwayFromVC()
+                    case .failure(let error):
+                        print(error)
+                    }
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    @objc private func addImagePressed(){
+        switch PHPhotoLibrary.authorizationStatus(){
+        case .notDetermined, .denied, .restricted:
+            PHPhotoLibrary.requestAuthorization({[weak self] status in
+                switch status {
+                case .authorized:
+                    self?.presentPhotoPickerController()
+                case .denied:
+                    print("Denied photo library permissions")
+                default:
+                    print("no status")
+                }
+            })
+        default: presentPhotoPickerController()
+        }
+    }
+    
+    @objc private func cancelPressed(){
+           dismiss(animated: true, completion: nil)
+       }
+ 
+//MARK: -Private Methods
+    
+    private func showAlert(with title: String, and message: String){
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        present(alertVC, animated: true, completion: nil)
+    }
+    
+    private func presentPhotoPickerController(){
+        DispatchQueue.main.async {
+            let imagePickerViewController = UIImagePickerController()
+            //imagePickerViewController.delegate = self
+            imagePickerViewController.sourceType = .photoLibrary
+            imagePickerViewController.allowsEditing = true
+            imagePickerViewController.mediaTypes = ["public.image"]
+            self.present(imagePickerViewController, animated: true, completion: nil)
+        }
+    }
+    
+    private func handleNavAwayFromVC(){
+        if settingFromLogin{
+            guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+                let sceneDelegate = windowScene.delegate as? SceneDelegate, let window = sceneDelegate.window
+                else {
+                    print("cant transition")
+                    return
+            }
+            UIView.transition(with: window, duration: 0.3, options: .transitionFlipFromBottom, animations: {
+                window.rootViewController = PursuitTabBar()
+            }, completion: nil)
+        } else {
+            self.navigationController?.popViewController(animated: true)
+        }
+    }
+    
+  
+//MARK: -UI Constraints
+//MARK: - Extensions
+    
 }
+
+
 /*
 import UIKit
 import Photos
@@ -99,91 +190,9 @@ class ProfileEditViewController: UIViewController {
         //MARK: TODO - load in user image and fields when coming from profile page
     }
     
-    @objc private func savePressed(){
-        guard let userName = userNameTextField.text, let imageURL = imageURL else {
-            //MARK: TODO - alert
-            return
-        }
-        
-        FirebaseAuthService.manager.updateUserFields(userName: userName, photoURL: imageURL) { (result) in
-            switch result {
-            case .success():
-                FirestoreService.manager.updateCurrentUser(userName: userName, photoURL: imageURL) { [weak self] (nextResult) in
-                    switch nextResult {
-                    case .success():
-                        self?.handleNavigationAwayFromVC()
-                    case .failure(let error):
-                        //MARK: TODO - handle
-                        
-                        //Discussion - if can't update on user object in collection, our firestore object will not match what is in auth. should we:
-                        // 1. Re-try the save?
-                        // 2. Revert the changes on the auth user?
-                        // This reconciliation should all be handled on the server side, but having to handle here, we could run into an infinite loop when re-saving.
-                        print(error)
-                    }
-                }
-            case .failure(let error):
-                //MARK: TODO - handle
-                print(error)
-            }
-        }
-    }
+  
     
-    @objc private func addImagePressed() {
-        //MARK: TODO - action sheet with multiple media options
-        switch PHPhotoLibrary.authorizationStatus() {
-        case .notDetermined, .denied, .restricted:
-            PHPhotoLibrary.requestAuthorization({[weak self] status in
-                switch status {
-                case .authorized:
-                    self?.presentPhotoPickerController()
-                case .denied:
-                    //MARK: TODO - set up more intuitive UI interaction
-                    print("Denied photo library permissions")
-                default:
-                    //MARK: TODO - set up more intuitive UI interaction
-                    print("No usable status")
-                }
-            })
-        default:
-            presentPhotoPickerController()
-        }
-    }
-    
-    private func showAlert(with title: String, and message: String) {
-        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-        present(alertVC, animated: true, completion: nil)
-    }
-    
-    private func presentPhotoPickerController() {
-        DispatchQueue.main.async{
-            let imagePickerViewController = UIImagePickerController()
-            imagePickerViewController.delegate = self
-            imagePickerViewController.sourceType = .photoLibrary
-            imagePickerViewController.allowsEditing = true
-            imagePickerViewController.mediaTypes = ["public.image"]
-            self.present(imagePickerViewController, animated: true, completion: nil)
-        }
-    }
-    
-    private func handleNavigationAwayFromVC() {
-        if settingFromLogin {
-            //MARK: TODO - refactor this logic into scene delegate
-            guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-                let sceneDelegate = windowScene.delegate as? SceneDelegate, let window = sceneDelegate.window
-                else {
-                    //MARK: TODO - handle could not swap root view controller
-                    return
-            }
-            UIView.transition(with: window, duration: 0.3, options: .transitionFlipFromBottom, animations: {
-                window.rootViewController = RedditTabBarViewController()
-            }, completion: nil)
-        } else {
-            self.navigationController?.popViewController(animated: true)
-        }
-    }
-    
+  
     private func setupViews() {
         setupImageView()
         setupUserNameTextField()
@@ -279,65 +288,6 @@ import Photos
 
 class ProfileEditViewController: UIViewController {
 
-    
-    lazy var imageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.backgroundColor = .init(white: 0.2, alpha: 0.9)
-        imageView.image = UIImage(systemName: "person")
-        imageView.tintColor = .white
-        return imageView
-    }()
-
-    lazy var addImageButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("Add Image", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-         button.titleLabel?.font = button.titleLabel?.font.withSize(34)
-        button.backgroundColor = .init(white: 0.2, alpha: 0.9)
-        button.addTarget(self, action: #selector(addImagePressed), for: .touchUpInside)
-        button.showsTouchWhenHighlighted = true
-        return button
-    }()
-
-   
-    
-    lazy var userNameTextField: UITextField = {
-        let textField = UITextField()
-        textField.placeholder = "Enter User Name"
-        textField.autocorrectionType = .no
-        textField.textAlignment = .left
-        textField.layer.cornerRadius = 15
-        textField.backgroundColor = .init(white: 1.0, alpha: 0.2)
-        textField.textColor = .white
-        textField.borderStyle = .roundedRect
-        textField.delegate = self
-        return textField
-    }()
-
-    lazy var saveButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("Save Profile", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.titleLabel?.font = UIFont(name: "System", size: 14)
-        button.backgroundColor = .init(white: 0.2, alpha: 0.9)
-        button.showsTouchWhenHighlighted = true
-        button.layer.cornerRadius = 5
-        button.addTarget(self, action: #selector(savePressed), for: .touchUpInside)
-        return button
-    }()
-    
-    lazy var cancelButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("Cancel", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.titleLabel?.font = UIFont(name: "System", size: 14)
-        button.backgroundColor = .init(white: 0.2, alpha: 0.9)
-        button.layer.cornerRadius = 5
-
-        button.addTarget(self, action: #selector(cancelButtonPressed), for: .touchUpInside)
-        return button
-    }()
-
     //MARK: Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -354,99 +304,13 @@ class ProfileEditViewController: UIViewController {
     }
 
     
-    //MARK: OBJC Methods
-    @objc private func savePressed(){
-        guard let userName = userNameTextField.text, let imageURL = imageURL else {
-            //MARK: TODO - alert
-            showAlert(with: "Failure", and: "Profile Not Updated")
-            return
-        }
-    
-
-        FirebaseAuthService.manager.updateUserFields(userName: userName, photoURL: imageURL) { (result) in
-            switch result {
-            case .success():
-                FirestoreService.manager.updateCurrentUser(userName: userName, photoURL: URL(string: imageURL)) { [weak self] (nextResult) in
-                    switch nextResult {
-                    case .success():
-                        self?.handleNavigationAwayFromVC()
-                    case .failure(let error):
-                        //MARK: TODO - handle
-
-                        //Discussion - if can't update on user object in collection, our firestore object will not match what is in auth. should we:
-                        // 1. Re-try the save?
-                        // 2. Revert the changes on the auth user?
-                        // This reconciliation should all be handled on the server side, but having to handle here, we could run into an infinite loop when re-saving.
-                        print(error)
-                    }
-                }
-            case .failure(let error):
-                //MARK: TODO - handle
-                print(error)
-            }
-        }
-    }
-    
-    @objc private func cancelButtonPressed(){
-        dismiss(animated: true, completion: nil)
-    }
-
-    @objc private func addImagePressed() {
-        //MARK: TODO - action sheet with multiple media options
-        switch PHPhotoLibrary.authorizationStatus() {
-        case .notDetermined, .denied, .restricted:
-            PHPhotoLibrary.requestAuthorization({[weak self] status in
-                switch status {
-                case .authorized:
-                    self?.presentPhotoPickerController()
-                case .denied:
-                    //MARK: TODO - set up more intuitive UI interaction
-                    print("Denied photo library permissions")
-                default:
-                    //MARK: TODO - set up more intuitive UI interaction
-                    print("No usable status")
-                }
-            })
-        default:
-            presentPhotoPickerController()
-        }
-    }
-
+  
     
     //MARK: Private Methods
-    private func showAlert(with title: String, and message: String) {
-        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-        present(alertVC, animated: true, completion: nil)
-    }
-
-    private func presentPhotoPickerController() {
-        DispatchQueue.main.async{
-            let imagePickerViewController = UIImagePickerController()
-            imagePickerViewController.delegate = self
-            imagePickerViewController.sourceType = .photoLibrary
-            imagePickerViewController.allowsEditing = true
-            imagePickerViewController.mediaTypes = ["public.image", "public.movie"]
-            self.present(imagePickerViewController, animated: true, completion: nil)
-        }
-    }
-
-    private func handleNavigationAwayFromVC() {
-        if settingFromLogin {
-            guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-                let sceneDelegate = windowScene.delegate as? SceneDelegate, let window = sceneDelegate.window
-                else {
-                    print("cant segue")
-                    return
-            }
-            UIView.transition(with: window, duration: 0.3, options: .transitionFlipFromBottom, animations: {
-                window.rootViewController = MainTabBarViewController()
-            }, completion: nil)
-        } else {
-            dismiss(animated: true, completion: nil)
-        }
-    }
     
+ 
+   
+   
     
     //MARK: Constraint Methods
 
